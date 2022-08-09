b6b5acd5a98843a09d38bd77fec7df7f0c29263b
RadotJar <reilly.hl12@gmail.com>
2022-08-09 16:21:55 +0930

    Implement argument parsing. Start formatting.

--- formatter/formatter.py
+++ formatter/formatter.py
@@ -1,43 +1,64 @@
 import sys
+import argparse
 
-fileName = sys.argv[1]
-inputFile = "./texts/" + fileName + ".txt"
-outputLines = []
-
-with open(inputFile, 'r') as f:
-    lines = f.readlines()
-
-for lineNum, line in enumerate(lines):
-    words = line.split('.')
+def main():
+    # Get input from command line
+    input = get_input()
     
-    # Remove words with uncertain characters
-    # words[:] = [word for word in words if '*' not in word and '!' not in word]
+    # Open file
+    file_path = "./texts/" + input["file_name"] + ".txt"
+    with open(file_path, "r") as file:
+        input_lines = file.readlines()
+    output_lines = []
 
-    # Reformat lines of words
-    for index, word in enumerate(words):
-        if '-' in word:
-            word = word.replace('-', '')
-        if '\n' in word:
-            word = word.replace('\n', '')
-        if '=' in word:
-            word = word.replace('=', '')
-        if '!' in word:
-            word = word.replace('!', '')
-        if '%' in word:
-            word = word.replace('%', '')
-        if '*' in word:
-            word = word.replace('*', '')
-        if index == len(words) - 1:
-            word = word + '\n'
-        words[index] = word
+    # Formatting
+    for line in input_lines:
+        # Start of line formatting
+            print(line[0])
+            # Comments
+            if(line[0] == "#"):
+                if(input["keepcomments"]):
+                    output_lines.append(line)
+                else:
+                    continue
+            # Locus data
+            elif(line[0] == "<" and line[1] == "f"):
+                if(input["locusraw"]):
+                    output_lines.append(line)
+                elif(input["locusproc"]):
+                    locus = ""
+                    i = 1
+                    while(line[i] != ">"):
+                        locus += line[i]
+                        i += 1
+                    output_lines.append(process_locus(locus))
+                else:
+                    continue
+            # Paragraph data
 
-    # Add all non-empty lines to output lines
-    if len(words) > 0:
-        outputLine = ' '.join(words)
-    else:
-        outputLine = ''
-    
-    outputLines.append(outputLine)
+def process_locus(locus):
+    print(locus)
+    return locus
 
-with open("./texts/" + fileName + "_formatted.txt", 'w') as f:
-    f.writelines(outputLines)
+def get_input():
+    parser = argparse.ArgumentParser(description="Voynich Manuscript Formatter", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("file_name", help="The name of the file to be formatted. The file must be a .txt file stored in ./texts/, containing a transcription of the Voynich Manuscript written in EVA and formatted in IVTFF.")
+    parser.add_argument("--keepcomments", action="store_true", help="Keep comments.")
+    locus_group = parser.add_mutually_exclusive_group()
+    locus_group.add_argument("--locusraw", action="store_true", help="Keep raw locus data.")
+    locus_group.add_argument("--locusproc", action="store_true", help="Keep processed locus data.")
+    parser.add_argument("--nospace", action="store_true", help="Keep '.' characters.")
+    parser.add_argument("--nouncertainspace", action="store_true", help="Remove ',' characters.")
+    parser.add_argument("--keepuncertain", action="store_true", help="Keep uncertain characters in their list format '[x:y:z]'.")
+    parser.add_argument("--nocapitallig", action="store_true", help="Do not convert ligatures in brace form to capital form.")
+    parser.add_argument("--nodrawingspaces", action="store_true", help="Remove '-' characters instead of converting to whitespace.")
+    paragraph_group = parser.add_mutually_exclusive_group()
+    paragraph_group.add_argument("--pararaw", action="store_true", help="Keep paragraph beggining (%) and end ($) characters in raw form.")
+    paragraph_group.add_argument("--paraproc", action="store_true", help="Process paragraph beggining (%) and end ($) into readable form.")
+    parser.add_argument("--noillegible", action="store_true", help="Words containing illegible characters (? | ???) are removed.")
+    args = parser.parse_args()
+    input = vars(args)
+    return input
+
+if __name__ == "__main__":
+    main()
